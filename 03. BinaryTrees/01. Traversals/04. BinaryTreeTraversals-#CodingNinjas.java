2698cda8eaf14a379a7099e0387a80e799d2f12a
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2025-01-24 14:14:10 +0530

    Update 04. BinaryTreeTraversals-#CodingNinjas.java

--- 03. BinaryTrees/01. Traversals/04. BinaryTreeTraversals-#CodingNinjas.java
+++ 03. BinaryTrees/01. Traversals/04. BinaryTreeTraversals-#CodingNinjas.java
@@ -24,18 +24,42 @@
  };
  ********************************************************/
 
-import java.util.List;
+import java.util.*;
 public class Solution {
     public static List<List<Integer>> getTreeTraversal(TreeNode root) {
         // Write your code here.
         List<List<Integer>> ans = new ArrayList<>();
-        ans.add(inorder(root));
-        ans.add(preorder(root));
-        ans.add(postorder(root));
+        ArrayList<Integer> curr = new ArrayList<Integer>();
+        inorder(root,curr);
+        ans.add(curr);
+
+        curr = new ArrayList<Integer>();
+        preorder(root,curr);
+        ans.add(curr);
+        curr = new ArrayList<Integer>();
+        postorder(root,curr);
+        ans.add(curr);
         return ans;
     }
-    public static List<Integer> inorder(TreeNode root){
+    public static void inorder(TreeNode root,List<Integer> curr){
         if(root==null) return;
-        
+        inorder(root.left,curr);
+        curr.add(root.data);
+        inorder(root.right,curr);
+        return;
+    }
+     public static void preorder(TreeNode root,List<Integer> curr){
+        if(root==null) return;
+        curr.add(root.data);
+        preorder(root.left,curr);
+        preorder(root.right,curr);
+        return;
+    }
+     public static void postorder(TreeNode root,List<Integer> curr){
+        if(root==null) return;
+        postorder(root.left,curr);
+        postorder(root.right,curr);
+        curr.add(root.data);
+        return;
     }
 }
