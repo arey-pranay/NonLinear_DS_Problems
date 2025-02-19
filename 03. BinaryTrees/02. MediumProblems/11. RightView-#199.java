95de19b7639273931f52404384ede75c9770e8e7
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2025-02-19 19:30:39 +0530

    Update 11. RightView-#199.java

--- 03. BinaryTrees/02. MediumProblems/11. RightView-#199.java
+++ 03. BinaryTrees/02. MediumProblems/11. RightView-#199.java
@@ -31,3 +31,36 @@ class Solution {
         return l;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        if(root==null) return ans;
+        q.add(root);
+        while(!q.isEmpty()){
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                TreeNode curr = q.remove();
+                if(curr.left!=null) q.add(curr.left);
+                if(curr.right!=null) q.add(curr.right);
+                if(i==sz-1) ans.add(curr.val);
+            }
+        }
+        return ans;
+    }
+}
