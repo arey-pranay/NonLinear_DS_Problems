812ce8b0849dc2f72d42cd379f7ad2d085329123
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2025-05-24 20:31:05 +0530

    Create 14. CourseReg_II-#210.java

---
+++ 05. Graphs/02. ProblemsOnBFS_DFS/14. CourseReg_II-#210.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[numCourses];
+
+        // Initialize the graph
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // Build graph and in-degree array
+        for (int[] pair : prerequisites) {
+            int course = pair[0];
+            int prereq = pair[1];
+            graph.get(prereq).add(course);
+            inDegree[course]++;
+        }
+
+        // Add courses with in-degree 0 to the queue
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if (inDegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        // Perform BFS
+        int[] result = new int[numCourses];
+        int index = 0;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            result[index++] = current;
+
+            for (int neighbor : graph.get(current)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.offer(neighbor);
+                }
+            }
+        }
+
+        // If not all courses are processed, return empty array
+        return index == numCourses ? result : new int[0];
+    }
+}
