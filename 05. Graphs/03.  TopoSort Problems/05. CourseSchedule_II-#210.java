a4adea269361d8e769676860c464ff0f4df777f3
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2025-06-17 19:29:39 +0530

    Create 05. CourseSchedule_II-#210.java

---
+++ 05. Graphs/03.  TopoSort Problems/05. CourseSchedule_II-#210.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
+        int[] indegree = new int[numCourses];
+        for(int[] pair : prerequisites){
+            graph.get(pair[1]).add(pair[0]);
+            indegree[pair[0]]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<numCourses;i++) if(indegree[i]==0) q.add(i);
+        int[] ans = new int[numCourses];
+        if(q.isEmpty()) return new int[]{};
+        int index=0;
+        while(!q.isEmpty()){
+            int sz = q.size();
+            for(int i=0; i<sz; i++){
+                int curr = q.remove();
+                for(int j: graph.get(curr)){
+                    indegree[j]--;
+                    if(indegree[j]==0) q.add(j);
+                }
+                ans[index++] = curr;
+            }
+        }
+        if(index!=numCourses) return new int[]{};
+        return ans;
+    }
+}
