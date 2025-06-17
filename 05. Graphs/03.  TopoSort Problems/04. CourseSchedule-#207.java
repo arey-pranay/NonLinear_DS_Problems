c83a2633df8a5a021dc59fe29b55b82fc38da032
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2025-06-17 19:33:24 +0530

    Update 04. CourseSchedule-#207.java

--- 05. Graphs/03.  TopoSort Problems/04. CourseSchedule-#207.java
+++ 05. Graphs/03.  TopoSort Problems/04. CourseSchedule-#207.java
@@ -1,28 +1,31 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        if (prerequisites.length == 0)
+            return true;
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<numCourses;i++) graph.add(new ArrayList<>());
+        for (int i = 0; i < numCourses; i++)
+            graph.add(new ArrayList<>());
         int[] indegree = new int[numCourses];
-        for(int[] pair : prerequisites){
-            graph.get(pair[0]).add(pair[1]);
-            indegree[pair[1]]++;
+        for (int[] pair : prerequisites) {
+            graph.get(pair[1]).add(pair[0]);
+            indegree[pair[0]]++;
         }
         Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<numCourses;i++) if(indegree[i]==0) q.add(i);
-        if(q.isEmpty()) return false;
-        HashSet<Integer> vis = new HashSet<>();
-        while(vis.size()!=numCourses){
-            int sz = q.size();
-            if(sz==0) return false;
-            for(int i=0; i<sz; i++){
-                int curr = q.remove();
-                vis.add(curr);
-                for(int j : graph.get(curr)){
-                    indegree[j]--;
-                    if(indegree[j] == 0) q.add(j);
-                }
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                q.add(i);
+        if (q.isEmpty())
+            return false;
+        int count = 0;
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            for (int j : graph.get(curr)) {
+                indegree[j]--;
+                if (indegree[j] == 0)
+                    q.add(j);
             }
+            count++;
         }
-        return true;
+        return count == numCourses;
     }
 }
